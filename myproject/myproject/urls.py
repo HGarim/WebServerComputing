6b6e79f27931bc85fd26449572e743f933068c27
HGarim <105338887+HGarim@users.noreply.github.com>
2025-05-09 18:33:13 +0900

    Update urls.py

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -5,8 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('request_test.urls')),
+    #path('', include('request_test.urls')),
+    path('', include('accounts.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
